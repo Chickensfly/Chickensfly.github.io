d9e34640a852be7f61ffad79aad8c6c5d1fdb49c
Jeff L <jeff300.liu@gmail.com>
2024-08-26 16:30:51 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const upperj = document.getElementById("upperj");
+const lowere1 = document.getElementById("lowere1");
+const lowerf1 = document.getElementById("lowerf1");
+const lowerf2 = document.getElementById("lowerf2");
+const lowerr = document.getElementById("lowerr");
+const lowere2 = document.getElementById("lowere2");
+const lowery = document.getElementById("lowery");
+const upperl = document.getElementById("upperl");
+const loweri = document.getElementById("loweri");
+const loweru = document.getElementById("loweru");
+let id = setInterval(frame, 10);
+
+function enlargeImg(x) {
+    x.style.transform = "scale(1.3)";
+}
+function normalizeImg(x) {
+    x.style.transform = "scale(1)";
+}
+
+
+
